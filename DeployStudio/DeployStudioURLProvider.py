a8a732fcd543966159c9b495b86780586eacd513
Jeremy Reichman <jaharmi@jaharmi.com>
2013-10-02 23:10:12 -0400

    Update DeployStudio static URL provider to find latest version.

--- DeployStudio/DeployStudioURLProvider.py
+++ DeployStudio/DeployStudioURLProvider.py
@@ -1,4 +1,5 @@
-import re
+#!/usr/bin/env python
+
 import urllib2
 
 from autopkglib import Processor, ProcessorError
@@ -6,9 +7,13 @@ from autopkglib import Processor, ProcessorError
 
 __all__ = ["DeployStudioURLProvider"]
 
-
+# URL to consult for current version of DeployStudio
 # http://www.deploystudio.com/Downloads/_dss.current
+update_url = "http://www.deploystudio.com/Downloads/_dss.current"
+
+# Sample URL to download a specific version of DeployStudio
 # http://www.deploystudio.com/get.php?fp=DeployStudioServer_v1.6.3.dmg
+
 
 class DeployStudioURLProvider(Processor):
     description = "Provides URL to the latest DeployStudio download."
@@ -18,19 +23,60 @@ class DeployStudioURLProvider(Processor):
         "version": {
             "description": "Version of the DeployStudio download.",
         },
+        "filename": {
+            "description": "Filename of the latest DeployStudio release download.",
+        },
         "url": {
             "description": "URL to the latest DeployStudio release download.",
         },
     }
-    
+
     __doc__ = description
-    
-    def main(self):        
-        self.env["version"] = "1.6.3"
+
+    def get_deploystudio_version(self, update_url):
+        """Find the latest version of DeployStudio and output as a string."""
+        try:
+            f = urllib2.urlopen(update_url)
+            html = f.read()
+            # The version is currently the only string in the text file at the
+            # update URL, so capture that string
+            # If this should change in the future or more error checking is desired,
+            # perform more processing here
+            download_version_string = html.strip()
+            f.close()
+        except BaseException as e:
+            raise ProcessorError("Can't download %s: %s" % (update_url, e))
+        return download_version_string
+
+    def get_deploystudio_dmg_file(self, download_version):
+        """Construct the name of the DeployStudio disk image file used in
+        the download URL."""
+        dmg_filename = "DeployStudioServer_v{0}.dmg".format(download_version)
+        return dmg_filename
+
+    def get_deploystudio_dmg_url(self, download_filename):
+        """Construct the URL for the DeployStudio file download."""
+        # Return URL
+        dmg_url = "http://www.deploystudio.com/get.php?fp={0}".format(download_filename)
+        return dmg_url
+
+    def main(self):
+        """Find and return a download URL."""
+
+        # Get the string representing the requested DeployStudio version
+        download_version = self.get_deploystudio_version(update_url)
+        self.env["version"] = download_version
         self.output("Found version %s" % self.env["version"])
-        self.env["filename"] = "DeployStudioServer_v1.6.3.dmg"
+
+        # Get the filename of the DeployStudio disk image download using the version
+        download_filename = self.get_deploystudio_dmg_file(download_version)
+        self.env["filename"] = download_filename
         self.output("Found download filename %s" % self.env["filename"])
-        self.env["url"] = "http://www.deploystudio.com/get.php?fp=DeployStudioServer_v1.6.3.dmg"
+
+        # Get the URL of the DeployStudio disk image download using the filename
+        download_url = self.get_deploystudio_dmg_url(download_filename)
+
+        self.env["url"] = download_url
         self.output("Found URL %s" % self.env["url"])
 
 
